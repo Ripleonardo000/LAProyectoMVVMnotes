ba0d570d95ee5d6257389b83c815303529ab27ad
Ripleonardo000 <leonardoandrade2003.197@gmail.com>
2024-06-16 10:58:42 -0500

    Agregar archivos de proyecto.

---
+++ LAProyectoMVVM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LAProyectoMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
