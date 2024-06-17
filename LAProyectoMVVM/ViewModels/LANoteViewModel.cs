14711044cfd0d53623e70d37bdc2b2cba267b1c2
Ripleonardo000 <leonardoandrade2003.197@gmail.com>
2024-06-16 20:24:46 -0500

    Actualizacion del modelo Creación del modelo de vista Acerca de Crea el modelo de vista nota

---
+++ LAProyectoMVVM/ViewModels/LANoteViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
+
+namespace LAProyectoMVVM.ViewModels
+{
+    internal class LANoteViewModel : ObservableObject, IQueryAttributable
+    {
+        private Models.LANote _note;
+    }
+}
--- LAProyectoMVVM/Views/LAAboutPage.xaml.cs
+++ LAProyectoMVVM/Views/LAAboutPage.xaml.cs
@@ -7,12 +7,5 @@ public partial class LAAboutPage : ContentPage
        InitializeComponent();
     }
 
-    private async void LearnMore_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.LAAbout about)
-        {
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-        }
-            
-    }
+   
 }
